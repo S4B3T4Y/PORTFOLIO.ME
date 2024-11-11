aa430df225919803f035710551b37ab765e31a28
S4B3T4Y <fsabetay@uade.edu.ar>
2024-11-11 18:48:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -67,3 +67,18 @@ function validarFormulario() {
     alert("Formulario enviado correctamente.");
     return true;
 }
+
+window.onscroll = function() {
+    const scrollToTopButton = document.getElementById("scrollToTop");
+    if (document.documentElement.scrollTop > 100) {
+        scrollToTopButton.style.display = "block";
+    } else {
+        scrollToTopButton.style.display = "none";
+    }
+};
+
+document.getElementById("scrollToTop").onclick = function(event) {
+    event.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
+
