4362d65a9fa54dd72a9febf145b4e86d2d5c62d9
S4B3T4Y <fsabetay@uade.edu.ar>
2024-11-11 11:50:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 let menuVisible = false;
 
-// Función que oculta o muestra el menú
+
 function mostrarOcultarMenu() {
     const nav = document.getElementById("nav");
     nav.classList.toggle("responsive", !menuVisible);
     menuVisible = !menuVisible;
 }
 
-// Función que oculta el menú una vez que se selecciona una opción
+
 function seleccionar() {
     document.getElementById("nav").classList.remove("responsive");
     menuVisible = false;
 }
 
-// Función que aplica las animaciones de las habilidades
+
 function efectoHabilidades() {
     const skills = document.getElementById("skills");
     const distanciaSkills = window.innerHeight - skills.getBoundingClientRect().top;
@@ -33,8 +33,8 @@ function efectoHabilidades() {
     }
 }
 
-// Detectar el evento de scroll para aplicar la animación de la barra de habilidades
+
 window.addEventListener("scroll", efectoHabilidades);
 
-// También, invocar la animación cuando la página se carga completamente
+
 document.addEventListener("DOMContentLoaded", efectoHabilidades);
